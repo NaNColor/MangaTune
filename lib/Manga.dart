5109b2fe50f5f00e68df75d897cf1de15e27d3a4
Rosamon <55803598+Rosamon@users.noreply.github.com>
2023-06-21 21:11:04 +0300

    Added a nature view and list of manga as example

--- lib/Manga.dart
+++ lib/Manga.dart
@@ -2,10 +2,19 @@
 
 class Manga {
   late String title;
-  late String image;
-  late int views;
+  //late String image;
+  late int likes;
   late String docId;
-
+  late String url;
+  late String date;
+  Manga({required titleManga, required urlManga, required dateManga})
+  {
+    title = titleManga;
+    url = urlManga;
+    date = dateManga;
+    likes = 0;
+    docId = "123";
+  }
   // Manga.fromDoc(QueryDocumentSnapshot doc) {
   //   title = doc["title"];
   //   image = doc["image"];
